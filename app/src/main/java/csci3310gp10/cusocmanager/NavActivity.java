a0386e382332cde6cbec9c76b7e8e2adf7cd8716
ACMYOKS <acmyoks1412@gmail.com>
2017-12-19 19:11:09 +0800

    Modified login page layout

--- app/src/main/java/csci3310gp10/cusocmanager/NavActivity.java
+++ app/src/main/java/csci3310gp10/cusocmanager/NavActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
